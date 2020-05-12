d4d7e0a946709538afef7eac2f51f211821215e9
Juan Quiroga <juan.quiroga@globant.com>
2020-05-12 15:55:23 -0500

    Initial Commit

---
+++ FalabellaFinancieroTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FalabellaFinancieroTest
+//
+//  Created by Juan Guillermo Quiroga Salamanca on 5/12/20.
+//  Copyright © 2020 Globant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
